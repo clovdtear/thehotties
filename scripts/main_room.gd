b65c923bcdc615d915e43bbaa88e421874c78496
daniele <clovdtear@gmail.com>
2026-07-15 09:47:40 +0300

    Navigation by pressing left/right arrows
    - When you press right arrow, it turns you to the wall to your right
    - When you press left arrow, it turns you to the wall to your left
    - Arrows have different images to indicate if it is in normal, hover, pressed state

---
+++ scripts/main_room.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+var walls = [
+	preload("res://scenes/walls/wall_front.tscn"),
+	preload("res://scenes/walls/wall_right.tscn"),
+	preload("res://scenes/walls/wall_back.tscn"),
+	preload("res://scenes/walls/wall_left.tscn")
+]
+
+var current_wall = 0
+var wall_instance
+
+func _ready():
+	change_wall(current_wall)
+
+func change_wall(number):
+	if wall_instance:
+		wall_instance.queue_free()
+
+	current_wall = number
+	wall_instance = walls[current_wall].instantiate()
+	$WallContainer.add_child(wall_instance)
+
+func _on_left_arrow_pressed() -> void:
+	var next_wall = current_wall - 1
+
+	if next_wall < 0:
+		next_wall = walls.size() - 1
+
+	change_wall(next_wall)
+
+func _on_right_arrow_pressed() -> void:
+	var next_wall = current_wall + 1
+
+	if next_wall >= walls.size():
+		next_wall = 0
+
+	change_wall(next_wall)
